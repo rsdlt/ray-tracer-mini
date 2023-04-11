3802ed26f53a91f468299f6a248e1a5d347154ca
rsdlt <rsdlt@pm.me>
2023-04-11 15:26:47 -0400

    initial setup main.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+const IMG_WIDTH: usize = 256;
+const IMG_HEIGHT: usize = 256;
+
+
+fn render(){
+   println!("P3\n {} {} \n255\n", IMG_WIDTH, IMG_HEIGHT) ;
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    render();
 }
