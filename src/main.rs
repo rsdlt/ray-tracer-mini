85c268b7b151849d99634c3e9131132fe78cbad3
rsdlt <rsdlt@pm.me>
2023-04-11 16:06:30 -0400

    first image in ppm is properly displayed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused_assignments, clippy::write_with_newline)]
+
 const IMG_WIDTH: usize = 256;
 const IMG_HEIGHT: usize = 256;
 
@@ -11,6 +13,24 @@ fn render() -> Result<File, std::io::Error> {
 
     let line = format!("P3\n{} {} \n255\n", IMG_WIDTH, IMG_HEIGHT);
     write!(img_file, "{}", line)?;
+
+    let (mut rf, mut gf, mut bf): (f64, f64, f64) = (0.0, 0.0, 0.0);
+    let (mut ri, mut gi, mut bi): (usize, usize, usize) = (0, 0, 0);
+    for j in 0..IMG_HEIGHT {
+        for i in 0..IMG_WIDTH {
+            (rf, gf, bf) = (
+                i as f64 / (IMG_WIDTH - 1) as f64,
+                j as f64 / (IMG_HEIGHT - 1) as f64,
+                0.25,
+            );
+            (ri, gi, bi) = (
+                (255.99 * rf) as usize,
+                (255.99 * gf) as usize,
+                (255.99 * bf) as usize,
+            );
+            write!(img_file, "{} {} {}\n", ri, gi, bi)?;
+        }
+    }
 
     Ok(img_file)
 }
