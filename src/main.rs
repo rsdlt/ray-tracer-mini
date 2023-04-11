895eb89063bd80368ba336f27b5594d2aef7880b
rsdlt <rsdlt@pm.me>
2023-04-11 15:44:44 -0400

    initial file write

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 const IMG_WIDTH: usize = 256;
 const IMG_HEIGHT: usize = 256;
 
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
 
-fn render(){
-   println!("P3\n {} {} \n255\n", IMG_WIDTH, IMG_HEIGHT) ;
+fn render() -> Result<File, std::io::Error> {
+    let path = Path::new("image.ppm");
+    let mut img_file = File::create(path)?;
+
+    let line = format!("P3\n {} {} \n255\n", IMG_WIDTH, IMG_HEIGHT);
+    write!(img_file, "{}", line)?;
+
+    Ok(img_file)
 }
 
 fn main() {
-
     render();
 }
