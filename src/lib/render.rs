1468358f55bc3bae35375a88ec66128708d1b2f2
rsdlt <rsdlt@pm.me>
2023-04-12 10:03:08 -0400

    Add basic rendering surface normals on sphere

--- src/lib/render.rs
+++ src/lib/render.rs
@@ -11,22 +11,30 @@ use crate::color::Color;
 use crate::ray::Ray;
 use crate::vector::{Point3, Vec3};
 
-fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin() - center;
     let a = Vec3::dot(r.direction(), r.direction());
     let b = 2.0 * Vec3::dot(oc, r.direction());
     let c = Vec3::dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
 
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &r) {
-        return Color::red();
+    let mut t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, &r);
+
+    if t > 0.0 {
+        let n = Vec3::unit(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x + 1.0, n.y + 1.0, n.z + 1.0);
     }
+
     let unit_direction = r.direction().to_unit();
-    let t = 0.5 * (unit_direction.y + 1.0);
+    t = 0.5 * (unit_direction.y + 1.0);
 
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
